100fdf32671008eaa4a807a996470439ca805e76
Diego Rapichan <direrapichan@gmail.com>
2026-01-20 11:45:07 -0300

    Clean up comments in ClienteService interface
    Removed unnecessary comment about changes.

--- src/main/java/one/digitalinnovation/gof/service/ClienteService.java
+++ src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -6,7 +6,7 @@ import one.digitalinnovation.gof.model.Cliente;
  * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
  * isso, se necessário, podemos ter multiplas implementações dessa mesma
  * interface.
- * 
+ *  -- alteracções por Diego Rapichan
  * @author falvojr
  */
 public interface ClienteService {
