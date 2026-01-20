7cf980fde71c637620fafc1f44d6af0a9bd87a14
Diego Rapichan <direrapichan@gmail.com>
2026-01-20 11:46:44 -0300

    Add comment to ClienteRepository interface
    Added a comment to clarify the purpose of the repository.

--- src/main/java/one/digitalinnovation/gof/model/ClienteRepository.java
+++ src/main/java/one/digitalinnovation/gof/model/ClienteRepository.java
@@ -2,7 +2,7 @@ package one.digitalinnovation.gof.model;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+//ClienteRepositorio que extende CrudRepository
 @Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
 
