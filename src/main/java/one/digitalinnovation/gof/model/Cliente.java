ca01a14f4c028e8ea08e7a7576477ea621c43876
Diego Rapichan <direrapichan@gmail.com>
2026-01-20 11:45:59 -0300

    Update Cliente.java

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
+//classe Cliente
 @Entity
 public class Cliente {
 
